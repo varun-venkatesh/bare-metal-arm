db50374b456f63667a6cfc9879e3fbf8b4a1f7bb
Varun Venkatesh <64339205+varun-venkatesh@users.noreply.github.com>
2020-11-19 11:24:20 -0500

    Update example_tasks.c

--- src/chapter5/example_tasks.c
+++ src/chapter5/example_tasks.c
@@ -4,26 +4,26 @@
 #include "uart_drv.h"
 #include "serial_print.h"
 
-void example_task0(void)
-{
+/* Our example tasks don't do much other than:
+ * make note of the entry time in terms of systime_t and print this on the console
+ * loop until the task has run for 1000 systeim_t units since it entered
+ * print an exit message and return.
+ */
+
+void example_task0(void) {
     systime_t entry_time = system_time_get();
     serial_puts("example_task0 entered system_time: ");
     serial_put_uint(entry_time);
     serial_putchar('\n');
-    while(entry_time + 1000u > system_time_get()){
-
-    }
+    while(entry_time + 1000u > system_time_get()) ;
     serial_puts("example_task0 exits!\n");
 }
 
-void example_task1(void)
-{
+void example_task1(void) {
     systime_t entry_time = system_time_get();
     serial_puts("example_task1 entered system_time: ");
     serial_put_uint(entry_time);
     serial_putchar('\n');
-    while(entry_time + 1000u > system_time_get()){
-
-    }
+    while(entry_time + 1000u > system_time_get());
     serial_puts("example_task1 exits!\n");
 }
