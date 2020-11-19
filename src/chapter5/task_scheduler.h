cafcf2c019a8b78981bd57c9b820213e63dc3b0f
Varun Venkatesh <64339205+varun-venkatesh@users.noreply.github.com>
2020-11-19 11:05:57 -0500

    Added descriptive comments

--- src/chapter5/task_scheduler.h
+++ src/chapter5/task_scheduler.h
@@ -5,12 +5,18 @@
 /* Defining a function pointer type for a task's start function/routine */
 typedef void (*task_start_fptr)(void);
 
+/* Task descriptor that includes
+ * a pointer to the entry function of the task
+ * the duration of the task in systime_t units
+ * the last systime_t when the task was run
+ */
 typedef struct{
     task_start_fptr     start;
     systime_t           duration;
     systime_t           last_run;
 }task_desc;
 
+/* Error enumerations for the scheduler */
 typedef enum{
     SCHEDULER_OKAY = 0,
     SCHEDULER_TOO_MANY_TASKS
